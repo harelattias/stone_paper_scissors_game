4212476738f6b4cb0a65587a7354ac55880f916a
harelattias <109675030+harelattias@users.noreply.github.com>
2022-07-27 15:48:39 +0200

    activation by clap sound

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
-def כאשר():
-    pass
+def on_sound_loud():
+    global gg1
+    basic.show_number(3)
+    basic.pause(500)
+    basic.show_number(2)
+    basic.pause(500)
+    basic.show_number(1)
+    basic.pause(500)
+    gg1 = randint(1, 3)
+    if gg1 == 1:
+        basic.show_leds("""
+            . . # . .
+                        . # # # .
+                        # # # # #
+                        . # # # .
+                        . . # . .
+        """)
+    elif gg1 == 2:
+        basic.show_leds("""
+            # . . . #
+                        . # . # .
+                        . . # . .
+                        # # . # #
+                        # # . # #
+        """)
+    else:
+        if gg1 == 3:
+            basic.show_leds("""
+                # # # # #
+                                # # # # #
+                                # # # # #
+                                # # # # #
+                                # # # # #
+            """)
+input.on_sound(DetectedSound.LOUD, on_sound_loud)
+
+gg1 = 0
 basic.show_leds("""
     . . . . .
         . . . . .
@@ -8,14 +43,13 @@ basic.show_leds("""
         # . # . #
 """)
 music.play_melody("- - - C5 B A B C5 ", 120)
-
-def on_forever():
-    pass
-basic.forever(on_forever)
-
-def on_every_interval():
-    basic.pause(1500)
-    basic.show_number(input.temperature())
-    input.calibrate_compass()
-    basic.show_arrow(ArrowNames.NORTH)
-loops.every_interval(20000, on_every_interval)
+basic.pause(100)
+basic.show_leds("""
+    . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+""")
+input.set_sound_threshold(SoundThreshold.LOUD, 255)
+input.set_sound_threshold(SoundThreshold.QUIET, 254)
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-input.onSound(DetectedSound.Loud, function () {
+input.onSound(DetectedSound.Loud, function on_sound_loud() {
+    
     basic.showNumber(3)
     basic.pause(500)
     basic.showNumber(2)
@@ -9,47 +10,46 @@ input.onSound(DetectedSound.Loud, function () {
     if (gg1 == 1) {
         basic.showLeds(`
             . . # . .
-            . # # # .
-            # # # # #
-            . # # # .
-            . . # . .
-            `)
+                        . # # # .
+                        # # # # #
+                        . # # # .
+                        . . # . .
+        `)
     } else if (gg1 == 2) {
         basic.showLeds(`
             # . . . #
-            . # . # .
-            . . # . .
-            # # . # #
-            # # . # #
+                        . # . # .
+                        . . # . .
+                        # # . # #
+                        # # . # #
+        `)
+    } else if (gg1 == 3) {
+        basic.showLeds(`
+                # # # # #
+                                # # # # #
+                                # # # # #
+                                # # # # #
+                                # # # # #
             `)
-    } else {
-        if (gg1 == 3) {
-            basic.showLeds(`
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
-        }
     }
+    
 })
 let gg1 = 0
 basic.showLeds(`
     . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    # . # . #
-    `)
+        . . . . .
+        . . . . .
+        . . . . .
+        # . # . #
+`)
 music.playMelody("- - - C5 B A B C5 ", 120)
 basic.pause(100)
 basic.showLeds(`
     . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+`)
 input.setSoundThreshold(SoundThreshold.Loud, 255)
 input.setSoundThreshold(SoundThreshold.Quiet, 254)
